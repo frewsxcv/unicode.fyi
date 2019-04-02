bcadae937f32fde594d3d6618738ecd49d25b749
Corey Farwell <coreyf@rwell.org>
2019-04-02 21:18:04 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use unicode_segmentation::UnicodeSegmentation;
+use unic::ucd::{name_aliases_of, Name, NameAliasType};
+use wasm_bindgen::prelude::*;
+use std::io;
+
+#[wasm_bindgen]
+#[derive(Debug)]
+struct CharInfo {
+    char: char,
+    display: String,
+    name: String,
+}
+
+impl CharInfo {
+    fn from_char(c: char) -> Self {
+        CharInfo {
+            char: c,
+            display: char_display(c),
+            name: char_name(c),
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    for grapheme_cluster in buffer.graphemes(true) {
+        println!("cluster");
+        for char_info in grapheme_cluster.chars().map(CharInfo::from_char) {
+            println!("{:?}", char_info);
+        }
+    }
+
+    Ok(())
+}
+
+fn char_display(c: char) -> String {
+    if c.is_whitespace() {
+        c.escape_default().collect::<String>()
+    } else {
+        c.to_string()
+    }
+}
+
+fn char_name(c: char) -> String {
+    Name::of(c).map(|n| n.to_string()).unwrap_or_else(|| {
+        match name_aliases_of(c, NameAliasType::NameAbbreviations) {
+            Some(abbrs) => abbrs[0].to_owned(),
+            None => "<none>".to_owned(),
+        }
+    })
+}
