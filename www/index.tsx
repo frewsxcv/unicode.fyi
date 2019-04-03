013a1dfa72c279ebde94493172c5d6cb56b49ba3
Corey Farwell <coreyf@rwell.org>
2019-04-03 18:02:23 +0200

    typescript interface for rust types, and a flexbox

--- www/index.tsx
+++ www/index.tsx
@@ -6,16 +6,45 @@ console.info(wasm.unicode_info("Family! 👪"))
 
 const app = document.getElementById("app");
 
+interface Word {
+    content: string;
+    words: GraphemeCluster[];
+}
+
+interface GraphemeCluster {
+    content: string;
+    words: CodePoints[];
+}
+
+interface CodePoints {
+    age: string;
+    char: string;
+    display: string;
+    general_category: string;
+    grapheme_cluster_break: string;
+    is_alphabetic: boolean;
+    is_lowercase: boolean;
+    is_uppercase: boolean;
+    is_white_space: boolean;
+    name: string;
+}
+
+const unicodeInfo = (s: string): Word[] => {
+    return wasm.unicode_info(s) as Word[];
+};
+
 export const App = () => {
     const onInput = (evt: React.FormEvent<HTMLInputElement>) => {
         const value = (evt.target as HTMLInputElement).value || "";
-        console.log(wasm.unicode_info(value));
+        console.log(unicodeInfo(value));
     };
 
     return (
         <>
             <h1>Unicode FYI</h1>
             <input type="text" id="input" onInput={onInput} />
+            <div style={ { display: 'flex' } }>
+            </div>
         </>
     );
 };
