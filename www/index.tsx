0fe2ef04ee62e55989624e63b9586a0415a8b756
Corey Farwell <coreyf@rwell.org>
2019-04-03 16:16:13 +0200

    react!

--- www/index.tsx
+++ www/index.tsx
@@ -1,10 +1,24 @@
 import * as wasm from "unicode-fyi";
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
 
 console.info(wasm.unicode_info("Family! 👪"))
 
 const input = document.getElementById("input");
 
+const results = document.getElementById("results");
+
 input.addEventListener("input", evt => {
     const value = (evt.target as HTMLInputElement).value || "";
     console.log(wasm.unicode_info(value));
+    console.log(Hello());
 });
+
+export const Hello = () => {
+    return (<h1>Hello!</h1>);
+};
+
+ReactDOM.render(
+    <Hello />,
+    results
+);
