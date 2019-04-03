71e0ed72ce776c89235ee2133c90b59ff2bbaef3
Corey Farwell <coreyf@rwell.org>
2019-04-03 11:38:04 +0200

    add input field

--- www/index.js
+++ www/index.js
@@ -1,3 +1,10 @@
 import * as wasm from "unicode-fyi";
 
 console.info(wasm.unicode_info("Family! 👪"))
+
+const input = document.getElementById("input");
+
+input.addEventListener("input", evt => {
+    const value = evt.target.value || "";
+    console.log(wasm.unicode_info(value));
+});
