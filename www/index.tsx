63114f6b44b17b6f6ef96987820156e82e2da798
Corey Farwell <coreyf@rwell.org>
2019-04-04 10:15:06 +0200

    throw a g in there

--- www/index.tsx
+++ www/index.tsx
@@ -38,7 +38,7 @@ interface AppState {
 class App extends React.Component<{}, AppState> {
     constructor(props: {}) {
         super(props);
-        this.state = { inputValue: 'Family! 👨‍👨‍👧‍👧' };
+        this.state = { inputValue: 'g̈Family! 👨‍👨‍👧‍👧' };
     }
 
     render() {
