adf14f417bdadf1cadc1479c8465672b097c857d
Corey Farwell <coreyf@rwell.org>
2019-04-28 19:18:28 -0400

    fix triforce example

--- www/index.tsx
+++ www/index.tsx
@@ -41,7 +41,7 @@ interface AppState {
 
 const examples = [
   "𒀭𒄿𒋾𒀭𒂗𒍪", // Iddin-Sin (Sumerian cuneiform)
-  "\u{a0}\u{a0}\u{a0}\u{a0}▲\n▲\u{a0}▲▲\n▲\u{a0}▲", // triforce
+  "\u{a0}\u{a0}▲\n▲\u{a0}▲", // triforce
   "¡Amo a mi familia! ❤️ 👨‍👨‍👧‍👧",
   "“Arrr!” 🏴‍☠️",
   "раураӏ.com",
