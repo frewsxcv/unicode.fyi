02a74d33faad19d30aed907aa0b7e2c2fe26aef7
Corey Farwell <coreyf@rwell.org>
2020-01-19 21:36:20 -0500

    move examples to sep file

--- www/index.tsx
+++ www/index.tsx
@@ -1,6 +1,7 @@
 import * as wasm from "../pkg/unicode_fyi";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { examples } from "./examples";
 
 const app = document.getElementById("app");
 
@@ -38,61 +39,6 @@ interface AppState {
   inputValue: string;
   forceInput: boolean;
 }
-
-const examples = [
-  // “Iddin-Sin”
-  // [Script: Sumerian cuneiform]
-  "𒀭𒄿𒋾𒀭𒂗𒍪",
-  // “I am Darius the great king” from the Behistun inscription
-  // [Script: Old Persian cuneiform]
-  "𐏐 𐎠𐎭𐎶 𐏐 𐎭𐎠𐎼𐎹𐎺𐎢𐏁 𐏐 𐎧𐏁𐎠𐎹𐎰𐎡𐎹 𐏐 𐎺𐏀𐎼𐎣",
-  // “Here is Corb, son of Labraid”
-  // Ballyboodan Ogham Stone
-  // [Script: Ogham]
-  "᚛ᚉᚑᚏᚁᚔᚕᚑᚔᚋᚐᚊᚔᚂᚐᚏᚔᚇ᚜",
-  // “Kenojuak Ashevak”
-  // [Script: Inuktitut]
-  "ᕿᓐᓄᐊᔪᐊᖅ ᐋᓯᕙᒃ",
-  // “Alenush Terian”
-  // [Script: Armenian]
-  "Ալենուշ Տէրեան",
-  // First verse from Codex Assemanianus
-  // [Script: Glagolitic]
-  "Ⰽⱐⱀⰹⰳⱏⰹ ⱃ҃ⱁⰴⱄⱅⰲⰰ ⰻ҃ⱆ҃ⱈⰲⰰ · ⱄ҃ⱀⰰ ⰴ҃ⰴⰲⰰ · ⱄ҃ⱀⰰ Ⰰⰲⱃⰰⰰ̄ⰿⰾ̄ⱑ · ",
-  // “Tohu wa-bohu”
-  // [Script: Hebrew]
-  "תֹ֙הוּ֙ וָבֹ֔הוּ",
-  // “Hindi Zahra”
-  // [Script: Tamazight]
-  "ⵜⵉⵏⴰⵔⵉⵡⵉⵏ",
-
-  // “Jyothirgamaya”
-  // [Script: Braille]
-  "⠠⠚⠽⠕⠞⠓⠊⠗⠛⠁⠍⠁⠽⠁",
-
-  // Regional Indicator Symbols for Nordic countries
-  "🇩🇰🇫🇴🇬🇱🇮🇸🇳🇴🇸🇪",
-  // triforce
-  "\u{a0}\u{a0}▲\n▲\u{a0}▲",
-  // “tomato, tomato” IPA
-  // International Phonetic Alphabet (IPA)
-  "/təˈmeɪtoʊ təˈmɑːtəʊ/",
-  "¡Amo a mi familia! ❤️ 👨‍👨‍👧‍👧",
-  "“Arrr!” 🏴‍☠️",
-  // Cyrillic lookalikes homograph attack
-  "раураӏ.com",
-  // De Morgan's law
-  "¬(𝑃∨𝑄)⇔(¬𝑃)∧(¬𝑄)",
-  // Royal Flush
-  "🂺🂻🂽🂾🂱",
-  // Yahtzee straight
-  "⚁⚂⚃⚄⚅",
-  "♸ – Polystyrene",
-  "(╯°□°）╯︵ ┻━┻",
-  "ಠ_ಠ",
-  "¯\\_(ツ)_/¯",
-  "14 Street–Union Square\nTrains: ④⑤⑥ⓁⓃⓆⓇⓌ"
-];
 
 class App extends React.Component<{}, AppState> {
   constructor(props: {}) {
