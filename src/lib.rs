ce6e6a4046b4e03babc3a8bd88a84d3705312f09
Corey Farwell <coreyf@rwell.org>
2020-07-31 22:44:46 -0400

    bytes component has been coverted!

--- src/lib.rs
+++ src/lib.rs
@@ -232,19 +232,25 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <div>
-                <BytesComponent bytes=vec![0, 1] />
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <BytesComponent bytes=vec![0, 1] code_unit_byte_len=1 />
+                // <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                // <p>{ self.value }</p>
             </div>
         }
     }
 }
 
-struct BytesComponent(Vec<u8>);
+const EXPLORE_COMPONENT_INNER_BORDER_COLOR: &str = "b--black-20";
+
+struct BytesComponent {
+    bytes: Vec<u8>,
+    code_unit_byte_len: u8,
+}
 
 #[derive(Clone, yew::Properties)]
 struct BytesComponentProps {
     bytes: Vec<u8>,
+    code_unit_byte_len: u8,
 }
 
 impl Component for BytesComponent {
@@ -252,7 +258,10 @@ impl Component for BytesComponent {
     type Properties = BytesComponentProps;
 
     fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self(props.bytes)
+        Self {
+            bytes: props.bytes,
+            code_unit_byte_len: props.code_unit_byte_len,
+        }
     }
 
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
@@ -264,24 +273,27 @@ impl Component for BytesComponent {
     }
 
     fn view(&self) -> Html {
+        let inner = self
+            .bytes
+            .iter()
+            .map(|b| format_byte(*b, self.code_unit_byte_len))
+            .map(|b| html!{ <div class="pr2">{ b }</div> })
+            .collect::<Html>();
         html! {
-            // <div
-            // class={`f7 bt br ${EXPLORE_COMPONENT_INNER_BORDER_COLOR} pa3 h2 nowrap tc flex items-center code`}
-            // >
-            // {inner}
-            // </div>
+            <div class=format!("f7 bt br {} pa3 h2 nowrap tc flex items-center code", EXPLORE_COMPONENT_INNER_BORDER_COLOR)>
+            {inner}
+            </div>
         }
     }
 }
 
-// fn BytesComponent(utf) = (props: { bytes: number[] }) => {
-//   const bytes = props.bytes.map((n) => `0x${n.toString(16)}`);
-//   const inner = bytes.map(byte => {
-//     return (
-//       <div className={'pr2'}>{byte}</div>
-//     );
-//   });
-// }
+fn format_byte(byte: u8, code_unit_byte_len: u8) -> String {
+    match code_unit_byte_len {
+        1 => format!("{:#04x}", byte),
+        2 => format!("{:#06x}", byte),
+        _ => unimplemented!(),
+    }
+}
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
